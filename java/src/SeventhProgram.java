0bcce2d4d197973797ca4e8c80e8591bb4652e4e
ushikubohayato <ushikubohayato1107@gmail.com>
2026-07-23 21:31:58 +0900

    second commit

--- java/src/SeventhProgram.java
+++ java/src/SeventhProgram.java
@@ -6,9 +6,8 @@ public class SeventhProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int num1 = sc.nextInt();
-		int num2 = sc.nextInt();
+		int num1 = Integer.parseInt(args[0]);
+		int num2 = Integer.parseInt(args[1]);
 		int result = num1 + num2;
 		System.out.println("加算の結果:" + result);
 
